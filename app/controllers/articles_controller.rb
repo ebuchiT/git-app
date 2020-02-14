701ac21a332fee4808d47375755a955aee81000c
ebuchiT <ebuchi.ebc@icloud.com>
2020-02-14 18:43:22 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -62,7 +62,6 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def article_params
       params.require(:article).permit(:title, :author, :text)
     end
